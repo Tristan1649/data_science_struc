47280901a5b1e80231c291f595c3d8075067fb6a
chingiz_bapanov <98455167+Tristan1649@users.noreply.github.com>
2022-11-09 09:16:04 +0600

    pie charts

---
+++ data_science_9.py
@@ -0,0 +1,7 @@
+data = [33, 25, 20, 12, 10] 
+plt.figure(num=1, 
+figsize=(6, 6)) 
+plt.axes(aspect=1) 
+plt.title('Plot 3', size=14) 
+plt.pie(data, labels=('Group 1', 'Group 2', 'Group 3', 'Group 4', 'Group 5')) 
+plt.show() 
